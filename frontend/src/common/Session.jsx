a2bf68eae4378435406a04c1e7afda600927f4b8
Vikas Kumawat <121717195+itsoksonu@users.noreply.github.com>
2025-05-30 18:06:17 +0530

    Update Session.jsx

--- frontend/src/common/Session.jsx
+++ frontend/src/common/Session.jsx
@@ -1,13 +1,13 @@
 const storeInSession = (key, data) => {
-    return sessionStorage.setItem(key, JSON.stringify(data));
- }
- 
- const lookInSession = (key) => {
-     return sessionStorage.getItem(key);
- }
- 
- const removeFromSession = (key) => {
-     return sessionStorage.removeItem(key);
- }
- 
- export {storeInSession, lookInSession, removeFromSession}
+    return localStorage.setItem(key, JSON.stringify(data));
+  };
+  
+  const lookInSession = (key) => {
+    return localStorage.getItem(key);
+  };
+  
+  const removeFromSession = (key) => {
+    return localStorage.removeItem(key);
+  };
+  
+  export { storeInSession, lookInSession, removeFromSession };
