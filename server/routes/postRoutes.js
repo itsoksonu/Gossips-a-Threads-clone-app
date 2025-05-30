c30708f66989514bfea6dfc3dce49086f8a70f00
Vikas Kumawat <121717195+itsoksonu@users.noreply.github.com>
2025-05-30 18:29:18 +0530

    Update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,23 +1,44 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import {likePost, createPost,  getHomeFeed, getUserPosts,  getPost, deletePost, repostPost, getPostLikes, getPostReposts, getPostQuotes, getPostActivity, getSavedPosts, toggleSavePost, } from "../controllers/postController.js";
+import {
+  likePost,
+  createPost,
+  getHomeFeed,
+  getUserPosts,
+  getPost,
+  deletePost,
+  repostPost,
+  getPostLikes,
+  getPostReposts,
+  getPostQuotes,
+  getPostActivity,
+  getSavedPosts,
+  toggleSavePost,
+  getDrafts,
+  saveDraft,
+  deleteDraft,
+  getLikedPosts,
+} from "../controllers/postController.js";
 import upload from "../config/multerConfig.js";
 
 const router = express.Router();
 
-router.get("/feed", protect, getHomeFeed); 
+router.get("/feed", protect, getHomeFeed);
 router.post("/create", protect, upload.array("media", 5), createPost);
 router.get("/saved-posts", protect, getSavedPosts);
-router.post("/:id/like", protect, likePost); 
-router.post("/:id/repost", protect, repostPost); 
+router.get("/liked-posts", protect, getLikedPosts);
+router.post("/save-draft", protect, upload.array("media", 5), saveDraft);
+router.get("/drafts", protect, getDrafts);
+router.post("/:id/like", protect, likePost);
+router.post("/:id/repost", protect, repostPost);
 router.delete("/:id", protect, deletePost);
 router.get("/:username", protect, getUserPosts);
-router.get("/post/:postId", protect, getPost); 
+router.delete("/draft/:id", protect, deleteDraft);
+router.get("/post/:postId", protect, getPost);
 router.get("/likes/:postId", protect, getPostLikes);
 router.get("/reposts/:postId", protect, getPostReposts);
 router.get("/quotes/:postId", protect, getPostQuotes);
 router.get("/activity/:postId", protect, getPostActivity);
 router.post("/save/:postId", protect, toggleSavePost);
-
 
 export default router;
