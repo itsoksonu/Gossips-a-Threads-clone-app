74b1bbf6ba417b0af040e056dbd66f5806aaada2
Vikas Kumawat <121717195+itsoksonu@users.noreply.github.com>
2025-05-30 18:27:10 +0530

    Update Message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -2,14 +2,12 @@ import { Schema, model } from "mongoose";
 
 const MessageSchema = new Schema({
   sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  receiver: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  content: { type: String, required: true },
+  receiver: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  senderUsername: { type: String, required: true }, 
+  receiverUsername: { type: String, required: true }, 
+  content: { type: String},
   isRead: { type: Boolean, default: false },
-  media: [{ type: String }],
+  media: [{ type: { type: String }, url: { type: String } }],
   createdAt: { type: Date, default: Date.now },
 });
 
