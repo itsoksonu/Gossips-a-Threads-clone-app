c61311b7039972310aaf2dd09a3f38554a523813
Vikas Kumawat <121717195+itsoksonu@users.noreply.github.com>
2025-05-30 18:06:42 +0530

    Update mobile-navbar.jsx

--- frontend/src/components/layouts/mobile-navbar.jsx
+++ frontend/src/components/layouts/mobile-navbar.jsx
@@ -4,7 +4,7 @@ import Navigation from '../navigations'
 export default function MobileNavbar({ layoutContext }) {
     return (
         <div className="fixed sm:hidden bottom-0 left-0 z-50 w-full h-16 backdrop-blur-xl bg-neutral-950">
-            <div className="grid h-full grid-cols-5 mx-auto w-full">
+            <div className="grid h-full grid-cols-6 mx-auto w-full">
                 <Navigation layoutContext={layoutContext} />
             </div>
         </div>
