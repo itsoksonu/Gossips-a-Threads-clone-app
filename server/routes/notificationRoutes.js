39e51e92cbe471f9bc75c0df20304db53fc64978
Vikas Kumawat <121717195+itsoksonu@users.noreply.github.com>
2025-05-30 18:29:05 +0530

    Update notificationRoutes.js

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getUserNotifications, markNotificationAsRead } from "../controllers/notificationController.js";
+import { getUserNotifications, markAllNotificationsAsRead } from "../controllers/notificationController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.get("/notifications", protect, getUserNotifications);
-router.put("/notifications/:notificationId/read", protect, markNotificationAsRead);
+router.put("/mark-all-read", protect, markAllNotificationsAsRead);
 
 export default router;
