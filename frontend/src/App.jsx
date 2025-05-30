9b8b2a67807b1b775bd7089f38d3c8c123b2815b
Vikas Kumawat <121717195+itsoksonu@users.noreply.github.com>
2025-05-30 18:20:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,9 +14,15 @@ import FollowRequests from "./components/FollowRequests";
 import { FollowProvider } from './contexts/FollowContext.jsx';
 import PostPage from "./pages/PostPage.jsx";
 import SavedPostsPage from "./pages/SavedPostsPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
+import LikedPostsPage from "./pages/LikedPostsPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import UserConversationPage from "./pages/UserConversationPage.jsx";
 
 function App() {
   const [userAuth, setUserAuth] = useState({ token: null, savedPosts: [] });
+  const [unreadNotificationCount, setUnreadNotificationCount] = useState(0);
 
   useEffect(() => {
     const userInSession = lookInSession("user");
@@ -27,6 +33,7 @@ function App() {
       } catch (error) {
         console.error("Failed to parse user data from session:", error);
         setUserAuth({ token: null });
+        removeFromSession("user");
       }
     }
   }, []);
@@ -40,19 +47,24 @@ function App() {
   }, [userAuth]);
 
   return (
-    <UserContext.Provider value={{ userAuth, setUserAuth }}>
+    <UserContext.Provider value={{ userAuth, setUserAuth, unreadNotificationCount, setUnreadNotificationCount }}>
       <FollowProvider>
         <Routes>
           <Route path="/" element={<ProtectedRoute> <Home /> </ProtectedRoute>} />
           <Route path="signup" element={<UserAuthForm type="signup" />} />
           <Route path="login" element={<UserAuthForm type="login" />} />
-          <Route path=":profileId" element={<ProfilePage/>} />
+          <Route path=":profileId" element={<ProfilePage />} />
           <Route path="/:username/post/:Postid" element={<ProtectedRoute> <PostPage /> </ProtectedRoute>} />
           <Route path="/search" element={<ProtectedRoute><SearchPage /></ProtectedRoute>} />
           <Route path="/activity" element={<ProtectedRoute><ActivityPage /></ProtectedRoute>} />
           <Route path="/followrequests" element={<ProtectedRoute><FollowRequests /></ProtectedRoute>} />
-          <Route path="/profile-setup" element={ <ProtectedRoute> <ProfileSetup /> </ProtectedRoute> } />
+          <Route path="/profile-setup" element={<ProtectedRoute> <ProfileSetup /> </ProtectedRoute>} />
           <Route path="/saved" element={<ProtectedRoute><SavedPostsPage /></ProtectedRoute>} />
+          <Route path="/liked" element={<ProtectedRoute><LikedPostsPage /></ProtectedRoute>} />
+          <Route path="/settings" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
+          <Route path="/chat" element={<ProtectedRoute><ChatPage/></ProtectedRoute>} />
+          <Route path="/chat/:username" element={<ProtectedRoute><UserConversationPage /></ProtectedRoute>} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} /> 
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </FollowProvider>
